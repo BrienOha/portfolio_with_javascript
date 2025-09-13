f0f33abb0d1e2ddec69a1711e4992c27026035d7
Bryan Jay Oja <bryanoja15@gmail.com>
2025-09-13 20:44:18 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// Dark Mode
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+// Color Changer
+const input = document.getElementById("colorPicker");
+
+const currentPrimary = getComputedStyle(document.documentElement)
+    .getPropertyValue("--primary-color")
+    .trim();
+
+input.value = currentPrimary;
+
+setColor();
+input.addEventListener("input", setColor);
+
+function setColor() {
+    document.documentElement.style.setProperty("--primary-color", input.value);
+}
+
+
+
+// Edit Feature
+const editBtn = document.createElement("button")
+editBtn.textContent = "Edit"
+editBtn.classList.add("edit-btn")
+
+const saveBtn = document.createElement("button")
+saveBtn.textContent = "Save"
+saveBtn.classList.add("save-btn")
+saveBtn.style.display = "none"
+
+document.body.appendChild(editBtn)
+document.body.appendChild(saveBtn)
+
+editBtn.addEventListener("click", () => {
+    document.querySelectorAll("section").forEach(section => {
+        section.contentEditable = "true"
+        section.classList.add("editing")
+    });
+    editBtn.style.display = "none"
+    saveBtn.style.display = "inline-block"
+})
+
+saveBtn.addEventListener("click", () => {
+    document.querySelectorAll("section").forEach(section => {
+        section.contentEditable = "false"
+        section.classList.remove("editing")
+    });
+    editBtn.style.display = "inline-block"
+    saveBtn.style.display = "none"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
